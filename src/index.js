801a6e1c2c3f53704bb333ca6003c33ae24a9150
Ranjeth <ranjeth1965@gmail.com>
2025-03-02 01:16:42 +0000

    Adding a player with keyboard controls & a game loop with creating cubes.

--- src/index.js
+++ src/index.js
@@ -100,3 +100,71 @@ canvas.addEventListener("mousemove", function (event) {
         }
     }
 });
+
+class Player {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.velX = 0;
+        this.velY = 0;
+        this.speed = 1;
+        this.friction = 0.98;
+        this.keys = {};
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        if (this.keys["ArrowUp"]) {
+            this.velY -= this.speed;
+        }
+        if (this.keys["ArrowDown"]) {
+            this.velY += this.speed;
+        }
+        if (this.keys["ArrowLeft"]) {
+            this.velX -= this.speed;
+        }
+        if (this.keys["ArrowRight"]) {
+            this.velX += this.speed;
+        }
+
+        this.velX *= this.friction;
+        this.velY *= this.friction;
+
+        this.x += this.velX;
+        this.y += this.velY;
+
+        if (this.x < 0) this.x = 0;
+        if (this.y < 0) this.y = 0;
+        if (this.x + this.width > canvas.width) this.x = canvas.width - this.width;
+        if (this.y + this.height > canvas.height) this.y = canvas.height - this.height;
+    }
+}
+
+const player = new Player(50, 50, 30, 30, "blue");
+
+document.addEventListener("keydown", (event) => {
+    player.keys[event.key] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+    player.keys[event.key] = false;
+});
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let square of squares) {
+        square.draw();
+    }
+    player.update();
+    player.draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
